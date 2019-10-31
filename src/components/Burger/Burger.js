32d96edab08b887aca8ab463411fc933d73b1d69
VinhNguyen <vinhnq@ocb.com.vn>
2019-10-31 13:16:19 +0700

    Frist Commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) =>{
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{
+        return [...Array(props.ingredients[igKey])].map((_, i)=>{
+          return <BurgerIngredient key={igKey + i} type={igKey} />  
+        });
+    });
+    return(
+        <div className={classes.Burger}>
+            {transformedIngredients}
+        </div>
+    );
+}
+export default burger;
